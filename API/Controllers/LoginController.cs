197f36092d00fdb6c16de68e65514cfc58183cd7
Georgy Petrov <georgy87petrov@yandex.ru>
2023-07-04 05:01:52 +0400

    feat - token sign

--- API/Controllers/LoginController.cs
+++ API/Controllers/LoginController.cs
@@ -1,12 +1,17 @@
 ﻿using API.Models;
 using API.Models.M4DModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Collections.Specialized;
 using System.Net;
 using System.Security.Cryptography;
+using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -30,7 +35,7 @@ namespace API.Controllers
             _cache = cache;
         }
 
-        [HttpGet]
+        [HttpGet("Login")]
         public async Task<IActionResult> Login(string login, string password)
         {
             _logger.LogInformation($"Income request Login : {login} - {password}");
@@ -65,6 +70,15 @@ namespace API.Controllers
             }
         }
 
+        [HttpGet("RefreshToken")]
+        public async Task<IActionResult> RefreshToken()
+        {
+            var  token = await GetNewToken();
+            var cacheOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromHours(1));
+            _cache.Set(TOKEN_CACHE_NAME, token, cacheOptions);
+            return Ok();
+        }
+
         private bool CheckCreditionals(string password, AppUser user) 
         {
             var inputHash = GetAppHash(password, user.Salt);
@@ -96,21 +110,42 @@ namespace API.Controllers
 
         private async Task<string> GetNewToken()
         {
-            var baseToken = await ExecuteM4DLoginCertRequest<GetAuthTokenResponse>();
+            var baseToken = await ExecuteM4DLoginCertRequest<GetAuthTokenResponse>("get");
 
             var sign = await GetSign(baseToken.base64Token);
 
-            var jwt = await ExecuteM4DLoginCertRequest<GetJwtResult>(baseToken.base64Token, sign);
+            var jwt = await ExecuteM4DLoginCertRequest<GetJwtResult>("post", baseToken.base64Token, sign);
             return jwt.token;
         }
 
         private async Task<string> GetSign(string base64Token)
         {
+            var encodedToken = Encoding.Default.GetString(Convert.FromBase64String(base64Token));
+            string Thumbprint;
 
-            return "123123";
+            var cms = new SignedCms(new ContentInfo(Encoding.Default.GetBytes(encodedToken)), true);
+
+            var _store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            _store.Open(OpenFlags.ReadOnly);
+
+
+            Thumbprint = _configuration.GetValue<string>("CertThumbprint");
+            var _cert = _store.Certificates.Find(X509FindType.FindByThumbprint, Thumbprint, false);
+
+            if (_cert.Count == 0)
+                throw new Exception($"К сожалению, не удалось найти сертификат с отпечатком {Thumbprint} в хранилище \"Личные\" компьютера, пожалуйста, произведите установку сертификата в хранилище");
+
+            var a = new CmsSigner(_cert[0]);
+
+            cms.ComputeSignature(a);
+
+            var _licenseText = cms.Encode();
+            return (Convert.ToBase64String(_licenseText));
+            
+            throw new Exception("Тело сертификат для роли \"edoSign\" не предоставлено.");
         }
 
-        private async Task<T> ExecuteM4DLoginCertRequest<T>(string token = "", string sign = "")
+        private async Task<T> ExecuteM4DLoginCertRequest<T>(string method, string token = "", string sign = "")
         {
             using var client = GetM4DClient();
 
@@ -120,8 +155,16 @@ namespace API.Controllers
                 content.Add(new StringContent(sign), "sign");
                 content.Add(new StringContent(token), "base64Token");
             }
+            HttpResponseMessage response;
+            if(method == "get")
+            {
+                response = await client.GetAsync("api/login/cert");
+            }
+            else
+            {
+                response = await client.PostAsync("api/login/cert", content);
+            }
 
-            var response = await client.PostAsync("api/login/cert", content);
             var body = await response.Content.ReadAsStringAsync();
             if (response.IsSuccessStatusCode)
             {
